07f662e9d70c4f097014ce0bf6e55ac146f18cdb
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2019-04-18 17:30:23 -0300

    configurando autentica jwt

--- App.js
+++ App.js
@@ -7,14 +7,28 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, AsyncStorage} from 'react-native';
 import Route from './src/view/route';
 
 export default class App extends Component<> {
+  state = {
+    signed: false
+  };
+
+  async componentDidMount() {
+    const user = JSON.parse(await AsyncStorage.getItem('me', null));
+    if(user){
+      this.setState({ signed: true })
+    }
+  }
+
   render() {
+    const { signLoaded, signed } = this.state;
+
+    const Layout = Route(signed);
     return (
       <View style={styles.container}>
-        <Route/>
+        <Layout/>
       </View>
     );
   }
--- src/view/route/index.js
+++ src/view/route/index.js
@@ -8,47 +8,49 @@ import Forgot from '../forgot';
 import Users from '../users';
 import Chat from '../chat';
 
+const Route = (signed) =>{
+  // Create the navigator
+  const Home = createStackNavigator({
+    Users: { screen: Users },
+    Chat: { screen: Chat }
+  },{
+    headerMode: 'screen',
+    mode: 'card',
+    initialRouteName: 'Users'
+  });
+  
+  // Create the switchNavigator
+  const switchNavigator = createSwitchNavigator({
+    Login: { screen: Login },
+    SignUp: { screen: SignUp },
+    Forgot: { screen: Forgot},
+    Home: { screen: Home }
+  },{
+    headerMode: 'screen',
+    mode: 'card',
+    initialRouteName: signed?'Home':'Login',
+  });
+  
+  /*
+  // Create the drawerNavigator
+  export const Drawer = createDrawerNavigator(
+    {
+  
+    },
+    {
+      initialRouteName: '',
+      drawerOpenRoute: 'DrawerOpen',
+      drawerCloseRoute: 'DrawerClose',
+      drawerToggleRoute: 'DrawerToggle',
+      //contentComponent: DrawerContent,
+      contentComponent: props => <SideBar {...props} />,
+      
+    }
+  );
+  */
+  
+  return createAppContainer(switchNavigator);
 
+}
 
-// Create the navigator
-const Home = createStackNavigator({
-  Users: { screen: Users },
-  Chat: { screen: Chat }
-},{
-  headerMode: 'screen',
-  mode: 'card',
-  initialRouteName: 'Users'
-});
-
-// Create the switchNavigator
-const switchNavigator = createSwitchNavigator({
-  Login: { screen: Login },
-  SignUp: { screen: SignUp },
-  Forgot: { screen: Forgot},
-  Home: { screen: Home }
-},{
-  headerMode: 'screen',
-  mode: 'card',
-  initialRouteName: 'Login',
-});
-
-/*
-// Create the drawerNavigator
-export const Drawer = createDrawerNavigator(
-  {
-
-  },
-  {
-    initialRouteName: '',
-    drawerOpenRoute: 'DrawerOpen',
-    drawerCloseRoute: 'DrawerClose',
-    drawerToggleRoute: 'DrawerToggle',
-    //contentComponent: DrawerContent,
-    contentComponent: props => <SideBar {...props} />,
-    
-  }
-);
-*/
-
-const Route = createAppContainer(switchNavigator);
 export default Route;
